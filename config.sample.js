88baf787769bd9f91a89cb3758deb638a511b1b1
stayfuntaxcol <stephan.tollenaar@gmail.com>
2025-09-04 08:59:01 +0200

    Create config.sample.js

---
+++ config.sample.js
@@ -0,0 +1,10 @@
+// config.sample.js
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID",
+  measurementId: "G-MEASUREMENT_ID"
+};
