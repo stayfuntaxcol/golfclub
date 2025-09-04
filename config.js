562f290a0346880c7bf6cd2cebed1dc4388afaca
stayfuntaxcol <stephan.tollenaar@gmail.com>
2025-09-04 08:55:45 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+// config.js
+window.__FIREBASE_CONFIG__ = {
+  apiKey: "AIzaSyD4gB9C0ZVcSLYQ4D4kM2-RtCb3ZJp3iag",
+  authDomain: "bestgolfclub-3425f.firebaseapp.com",
+  projectId: "bestgolfclub-3425f",
+  storageBucket: "bestgolfclub-3425f.appspot.com",
+  messagingSenderId: "219240386641",
+  appId: "1:219240386641:web:b38902c3a471a3b8442870",
+  measurementId: "G-G97G6PEJ2Q"
+};
